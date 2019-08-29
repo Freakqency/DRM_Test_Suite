e5a195794dae32504250302b89da86b4d02b6ee9
Freakqency <surya.16cs@kct.ac.in>
2019-08-29 17:34:00 +0000

    Implemented rest of the ioctls and fixed spacing

--- drm_test.c
+++ drm_test.c
@@ -195,176 +195,242 @@ int drm_destroy(int fd, struct drm_dev_t *dev_head)
 void drm_ioctl_version(int fd){
 	struct drm_version vers;
 
-	if (!ioctl(fd, DRM_IOCTL_VERSION, &vers))
-		warn("Version_major:%d,Version_minor:%d,Version_patchlevel:%d,Name_len:%d,\n",vers.version_major, vers.version_minor, vers.version_patchlevel,vers.name_len);
-	else
+	if (ioctl(fd, DRM_IOCTL_VERSION, &vers) == -1)
 		warn("drm_ioctl_version");
 
+	printf("Version_major:%d,Version_minor:%d,Version_patchlevel:%d,Name_len:%d,\n", vers.version_major, vers.version_minor, vers.version_patchlevel, vers.name_len);
 	printf("Struct Members :Version_major, Version_minor, Patchlevel, Name_len, Date_len, Desc_len\n");
 }
 
 void drm_ioctl_get_unique(int fd){
 	struct drm_unique unique;
-	if (!ioctl(fd, DRM_IOCTL_GET_UNIQUE, &unique))
-		warn("unique_len:%d,unique:%s",unique.unique_len, unique.unique);
-	else
+	if (ioctl(fd, DRM_IOCTL_GET_UNIQUE, &unique) == -1)
 		warn("error");
 	
+	printf("unique_len:%d,unique:%s", unique.unique_len, unique.unique);
 	printf("Struct members Unique_len,unique\n");
 }
 
 void drm_ioctl_set_unique(int fd){
 	struct drm_unique unique;
-	if (!ioctl(fd, DRM_IOCTL_SET_UNIQUE, &unique))
-		warn("unique_len:%d,unique:%s",unique.unique_len, unique.unique);
-	else
+	if (ioctl(fd, DRM_IOCTL_SET_UNIQUE, &unique) == -1)
 		warn("error");
 
+	printf("unique_len:%d,unique:%s", unique.unique_len, unique.unique);
 	printf("Struct members: Unique_len,unique\n");
 }
 
 void drm_ioctl_get_map(int fd){
 	struct drm_map map;
-	if (!ioctl(fd, DRM_IOCTL_GET_MAP, &map))
-		warn("offset:%ld,size:%ld,mtrr:%d",map.offset,map.size,map.mtrr);
-	else
+	if (ioctl(fd, DRM_IOCTL_GET_MAP, &map) == -1)
 		warn("error");
 	
+	printf("offset:%ld,size:%ld,mtrr:%d", map.offset, map.size, map.mtrr);
 	printf("Struct members: offset,size:,type,map_flag,handle,mtrr\n");
 }
 
 void drm_ioctl_add_map(int fd){
 	struct drm_map map;
-	if (!ioctl(fd, DRM_IOCTL_ADD_MAP, &map))
-		warn("offset:%ld,size:%ld,map_type:%d,map_flag%d,handle:%p,mtrr:%d",map.offset,map.size,map.type,map.flags,map.handle,map.mtrr);
-
-	else
+	if (ioctl(fd, DRM_IOCTL_ADD_MAP, &map) == -1)
 		warn("error");
 	
+	printf("offset:%ld,size:%ld,map_type:%d,map_flag%d,handle:%p,mtrr:%d", map.offset, map.size, map.type, map.flags, map.handle, map.mtrr);
 	printf("Struct members: offset,size,map_type,map_flag,handle,mtrr\n");
 }
 
 void drm_ioctl_rm_map(int fd){
 	struct drm_map map;
-	if (!ioctl(fd, DRM_IOCTL_RM_MAP, &map))
-		warn("offset:%ld,size:%ld,map_type:%d,map_flag%d,handle:%p,mtrr:%d",map.offset,map.size,map.type,map.flags,map.handle,map.mtrr);
-	else
+	if (ioctl(fd, DRM_IOCTL_RM_MAP, &map) == -1)
 		warn("error");
 
+	printf("offset:%ld,size:%ld,map_type:%d,map_flag%d,handle:%p,mtrr:%d", map.offset, map.size, map.type, map.flags, map.handle, map.mtrr);
 	printf("Struct members: offset,size,map_type,map_flag,handle,mtrr\n");
 }
 
 void drm_ioctl_get_client(int fd){
 	struct drm_client client;
-	if (!ioctl(fd, DRM_IOCTL_GET_CLIENT, &client))
-		warn("idx:%d,auth:%d,pid:%ld,uid:%ld,magic:%ld,iocs:%ld",client.idx,client.auth,client.pid,client.uid,client.magic,client.iocs);
-	else
+	if (ioctl(fd, DRM_IOCTL_GET_CLIENT, &client) == -1)
 		warn("error");
 
+	printf("idx:%d,auth:%d,pid:%ld,uid:%ld,magic:%ld,iocs:%ld", client.idx, client.auth, client.pid, client.uid, client.magic, client.iocs);
 	printf("Struct members: idx,auth,pid,uid,magic,iocs\n");
 }
 
 void drm_ioctl_get_stats(int fd){
 	struct drm_stats stats;
-	if (!ioctl(fd, DRM_IOCTL_GET_STATS, &stats))
-		warn("count:%ld",stats.count);
-	else
+	if (ioctl(fd, DRM_IOCTL_GET_STATS, &stats) == -1)
 		warn("error");
 	
+	printf("count:%ld",stats.count);
 	printf("Struct members: count\n");
 }
 
 void drm_ioctl_add_bufs(int fd){
 	struct drm_buf_desc desc;
 
-	if (!ioctl(fd, DRM_IOCTL_ADD_BUFS, &desc))
-		warn("count:%d,size:%d,low_mark:%d,high_mark:%d,flags:%d,agp_start:%ld",desc.count,desc.size,desc.low_mark,desc.high_mark,desc.flags,desc.agp_start);
-	else
+	if (ioctl(fd, DRM_IOCTL_ADD_BUFS, &desc) == -1)
 		warn("error");
 
+	printf("count:%d,size:%d,low_mark:%d,high_mark:%d,flags:%d,agp_start:%ld", desc.count, desc.size, desc.low_mark, desc.high_mark, desc.flags, desc.agp_start);
 	printf("Struct members: count,size,low_mark,high_mark,flags,agp_start\n");
 }
 
 void drm_ioctl_mark_bufs(int fd){
 	struct drm_buf_desc desc;
-	if (!ioctl(fd, DRM_IOCTL_MARK_BUFS, &desc))
-		warn("count:%d,size:%d,low_mark:%d,high_mark:%d,flags:%d,agp_start:%ld",desc.count,desc.size,desc.low_mark,desc.high_mark,desc.flags,desc.agp_start);
-	else
+	if (ioctl(fd, DRM_IOCTL_MARK_BUFS, &desc) == -1)
 		warn ("error");
 
+	printf("count:%d,size:%d,low_mark:%d,high_mark:%d,flags:%d,agp_start:%ld", desc.count, desc.size, desc.low_mark, desc.high_mark, desc.flags, desc.agp_start);
 	printf("Struct members: count,size,low_mark,high_mark,flags,agp_start\n");
 }
 
 void drm_ioctl_free_bufs(int fd){
 	struct drm_buf_desc desc;
-	if (!ioctl(fd, DRM_IOCTL_FREE_BUFS, &desc))
-		warn("count:%d,size:%d,low_mark:%d,high_mark:%d,flags:%d,agp_start:%ld",desc.count,desc.size,desc.low_mark,desc.high_mark,desc.flags,desc.agp_start);
-	else
+	if (ioctl(fd, DRM_IOCTL_FREE_BUFS, &desc) == -1)
 		warn("error");
 
+	printf("count:%d,size:%d,low_mark:%d,high_mark:%d,flags:%d,agp_start:%ld", desc.count, desc.size, desc.low_mark, desc.high_mark, desc.flags, desc.agp_start);
 	printf("Struct members: count,size,low_mark,high_mark,flags,agp_start\n");
 }
 
 void drm_ioctl_set_sarea_ctx(int fd){
 	struct drm_ctx_priv_map sarea;
 
-	if (!ioctl(fd, DRM_IOCTL_SET_SAREA_CTX, &sarea))
-		warn("ctx_id:%d,handle:%p",sarea.ctx_id,sarea.handle);
-	else
+	if (ioctl(fd, DRM_IOCTL_SET_SAREA_CTX, &sarea) == -1)
 		warn("error");
 	
+	printf("ctx_id:%d,handle:%p", sarea.ctx_id,sarea.handle);
 	printf("Struct members: ctx_id,handle\n");
 }
 
 void drm_ioctl_get_sarea_ctx(int fd){
 	struct drm_ctx_priv_map sarea;
 
-	if (!ioctl(fd, DRM_IOCTL_GET_SAREA_CTX, &sarea))
-		warn("ctx_id:%d,handle:%p",sarea.ctx_id,sarea.handle);
-	else
+	if (ioctl(fd, DRM_IOCTL_GET_SAREA_CTX, &sarea) == -1)
 		warn("error");
 		
+	printf("ctx_id:%d,handle:%p", sarea.ctx_id,sarea.handle);
 	printf("Struct members: ctx_id,handle\n");
 }
 
 void drm_ioctl_res_ctx(int fd){
 	struct drm_ctx_res res;
-	if (!ioctl(fd, DRM_IOCTL_RES_CTX, &res))
-		warn("count:%d",res.count);
-	else
+	if (ioctl(fd, DRM_IOCTL_RES_CTX, &res) == -1)
 		warn("error");
 
+	printf("count:%d", res.count);
 	printf("Struct members: count\n");
 }
 
 void drm_ioctl_dma(int fd){
 	struct drm_dma dma;
-	if (!ioctl(fd, DRM_IOCTL_DMA, &dma))
-		warn("context:%d",dma.context);
-	else
+	if (ioctl(fd, DRM_IOCTL_DMA, &dma) == -1)
 		warn("error");
 		
+	printf("context:%d", dma.context);
 	printf("Struct members: context\n");
 }
 
 void drm_ioctl_agp_enable(int fd){
 	struct drm_agp_mode agp;
-	if (!ioctl(fd, DRM_IOCTL_AGP_ENABLE, &agp))
-		printf("mode:%ld",agp.mode);
-	else
+	if (ioctl(fd, DRM_IOCTL_AGP_ENABLE, &agp) == -1)
 		warn("error");
 
+	printf("mode:%ld",agp.mode);
 	printf("Struct members: mode\n");
 }
 
 void drm_ioctl_agp_info(int fd){
 	struct drm_agp_info agp_info;
-	if (!ioctl(fd, DRM_IOCTL_AGP_INFO, &agp_info))
-		warn("agp_version_major:%d",agp_info.agp_version_major);
-	else
+	if (ioctl(fd, DRM_IOCTL_AGP_INFO, &agp_info) == -1)
 		warn("error");
 
+	printf("agp_version_major:%d", agp_info.agp_version_major);
 	printf("Struct members: agp_version_major\n");
+}
+
+void drm_ioctl_agp_alloc(int fd){
+	struct drm_agp_buffer agp_buffer;
+	if (ioctl(fd, DRM_IOCTL_AGP_ALLOC, &agp_buffer) == -1)
+		warn("error");
+
+	printf("agp_buffer_size:%ld, agp_buffer_handle:%ld, agp_buffer_type:%ld, agp_buffer_physical:%ld ", agp_buffer.size, agp_buffer.handle, agp_buffer.type, agp_buffer.physical);
+	printf("Struct membersagp_buffer_size, agp_buffer_handle, agp_buffer_type, agp_buffer_physical\n");
+}
+
+void drm_ioctl_agp_free(int fd){
+	struct drm_agp_buffer agp_buffer;
+	if (ioctl(fd, DRM_IOCTL_AGP_FREE, &agp_buffer) == -1)
+		warn("error");
+
+	printf("agp_buffer_size:%ld, agp_buffer_handle:%ld, agp_buffer_type:%ld, agp_buffer_physical:%ld ", agp_buffer.size, agp_buffer.handle, agp_buffer.type, agp_buffer.physical);
+	printf("Struct members: agp_buffer_size, agp_buffer_handle, agp_buffer_type, agp_buffer_physical\n");
+}
+
+void drm_ioctl_agp_bind(int fd){
+	struct drm_agp_binding agp_bind;
+	if (ioctl(fd, DRM_IOCTL_AGP_BIND, &agp_bind) == -1)
+		warn("error");
+
+	printf("agp_bind_handle:%ld, agp_bind_offset:%ld", agp_bind.handle, agp_bind.offset);
+	printf("Struct members: agp_bind_handle, agp_bind_offset\n");
+}
+
+void drm_ioctl_agp_unbind(int fd){
+	struct drm_agp_binding agp_unbind;
+	if (ioctl(fd, DRM_IOCTL_AGP_UNBIND, &agp_unbind) == -1)
+		warn("error");
+
+	printf("agp_unbind_handle:%ld, agp_unbind_offset:%ld", agp_unbind.handle, agp_unbind.offset);
+	printf("Struct members: agp_bind_handle, agp_bind_offset\n");
+}
+
+void drm_ioctl_sg_alloc(int fd){
+	struct drm_scatter_gather sg;
+	if (ioctl(fd, DRM_IOCTL_SG_ALLOC, &sg) == -1)
+		warn("error");
+
+	printf("sg_size:%ld, sg_handle:%ld", sg.size, sg.handle);
+	printf("Struct members: sg_size, sg_handle");
+}
+
+void drm_ioctl_sg_free(int fd){
+	struct drm_scatter_gather sg;
+	if (ioctl(fd, DRM_IOCTL_SG_FREE, &sg) == -1)
+		warn("error");
+
+	printf("sg_size:%ld, sg_handle:%ld", sg.size, sg.handle);
+	printf("Struct members: sg_size, sg_handle");
+}
+
+void drm_ioctl_update_draw(int fd){
+	struct drm_update_draw update_draw;
+	if (ioctl(fd, DRM_IOCTL_UPDATE_DRAW, &update_draw) == -1)
+		warn("error");
+
+	printf("update_draw_type:%d, update_draw_num:%d, update_draw_data:%lld", update_draw.type, update_draw.num, update_draw.data);
+	printf("Struct members: update_draw_type, update_draw_num, update_draw_data\n");
+
+}
+
+void drm_ioctl_wait_vblank(int fd){
+	union drm_wait_vblank vblank;
+	if (ioctl(fd, DRM_IOCTL_WAIT_VBLANK, &vblank) == -1)
+		warn("error");
+
+	printf("vblank_request_sequence:%d, vblank_request_signal:%ld, vblank_reply_sequence:%d", vblank.request.sequence, vblank.request.signal, vblank.reply.sequence);
+	printf("Struct members: vblank_request_sequence, vblank_request_signal, vblank_reply_sequence");
+}
+
+void drm_ioctl_addfb2(int fd){
+	struct drm_mode_fb_cmd2 addfb2;
+	if (ioctl(fd, DRM_IOCTL_MODE_ADDFB2, &addfb2) == -1)
+		warn("error");
+
+	printf("fb_id:%d, width:%d, height:%d, pixel_format:%d, flags:%d", addfb2.fb_id, addfb2.width, addfb2.height, addfb2.pixel_format, addfb2.flags);
+	for(int i = 0; i < 4; i++)
+	       printf("handles:%d, pitches:%d, offsets:%d, modifiers:%lld", addfb2.handles[i], addfb2.pitches[i], addfb2.offsets[i], addfb2.modifier[i]);
+	printf("Struct members: fb_id, width, height, pixel_format, flags, handles, pitches, offsets, modifiers");	
 }
 
 struct {
@@ -387,7 +453,16 @@ struct {
 	{"res_ctx", drm_ioctl_res_ctx},
 	{"dma", drm_ioctl_dma},
 	{"agp_enable", drm_ioctl_agp_enable},
-	{"agp_info", drm_ioctl_agp_info}
+	{"agp_info", drm_ioctl_agp_info},
+	{"agp_alloc", drm_ioctl_agp_alloc},
+	{"agp_free", drm_ioctl_agp_free},
+	{"agp_bind", drm_ioctl_agp_bind},
+	{"agp_unbind", drm_ioctl_agp_unbind},
+	{"sg_alloc", drm_ioctl_sg_alloc},
+	{"sg_free", drm_ioctl_sg_free},
+	{"update_draw", drm_ioctl_update_draw},
+	{"wait_vblank", drm_ioctl_wait_vblank},
+	{"mode_addfb2", drm_ioctl_addfb2}
 
 
 };
